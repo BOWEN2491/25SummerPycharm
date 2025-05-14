b1106661458d177d4c53443ad83ace044af59e49
BOWEN2491 <160082600+BOWEN2491@users.noreply.github.com>
2025-05-14 17:39:37 +0800

    done first part of third homework

--- app.py
+++ app.py
@@ -8,9 +8,16 @@ def index():
     return 'Hello, Flask!'
 
 
-@app.route('/hello/<name>')
-def hello(name):
-    return 'Hello, {}!'.format(name)
+@app.route('/hello/<username>/<password>')
+def hello(username, password):
+    dic = {'bowen':106}
+    result = "登陆失败"
+    tf = dic.__contains__(username)
+    value = dic.get(username)
+    tf2= dic.get(username)==password
+    if dic.__contains__(username)&(dic.get(username)==int(password)):
+        result = "登陆成功"
+    return result
 
 
 if __name__ == '__main__':
